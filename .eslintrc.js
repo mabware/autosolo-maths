8623ce64acba5e314559615feeb6923ac64f6519
Michael <michaelabartlett@googlemail.com>
2020-11-21 18:39:25 +0000

    add lintint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'jest',
+  ],
+  rules: {
+    'max-len': ['error', { code: 160 }],
+  },
+};
